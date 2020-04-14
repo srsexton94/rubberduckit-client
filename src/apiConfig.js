ddf3b9bda495bddf6653ddadfa8386ee5a7b8a11
srsexton94 <srsexton94@gmail.com>
2020-04-14 16:10:03 -0400

    Update production URL for new API
    Replaced Express API with a Rails API (for relational database), this
    changes the production url in the front end's config file

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,6 +1,6 @@
 let apiUrl
 const apiUrls = {
-  production: 'https://fast-badlands-56525.herokuapp.com/',
+  production: 'https://rubberduckit-api.herokuapp.com/ ',
   development: 'http://localhost:4741'
 }
 
